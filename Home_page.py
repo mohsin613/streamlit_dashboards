4ebed587f50d55ba11750d92e67c2dae5d76c62f
Syed Mohsin Abbas <106992534+mohsin613@users.noreply.github.com>
2022-11-11 18:52:09 +0500

    streamlit_app

--- Home_page.py
+++ Home_page.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import numpy as np
 st.set_page_config(
     page_title="National Statistic",
     page_icon="Data@Science"
@@ -7,6 +10,16 @@ st.set_page_config(
 #web app ka title
 st.markdown(''' 
     # ***National Statistic***
-This app is develop to generate rapid-fire EDA process using Python for ML Implementation
+    ---
+    This app is develop to generate rapid-fire EDA process using Python for ML Implementation
     ''')
+st.markdown("##### ***Display a Dataframe as an interactive table.***")
 st.sidebar.success("Select a page above")
+
+
+df = pd.DataFrame(px.data.gapminder())
+df = pd.DataFrame(
+   np.random.randn(50, 20),
+   columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df)
