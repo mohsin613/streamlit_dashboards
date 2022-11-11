99212260fad537aa0583d817df9e8a8a77639463
Syed Mohsin Abbas <106992534+mohsin613@users.noreply.github.com>
2022-11-11 17:20:55 +0500

    streamlit_app

---
+++ Home_page.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.set_page_config(
+    page_title="National Statistic",
+    page_icon="Data@Science"
+
+)
+#web app ka title
+st.markdown(''' 
+    # ***National Statistic***
+This app is develop to generate rapid-fire EDA process using Python for ML Implementation
+    ''')
+st.sidebar.success("Select a page above")
